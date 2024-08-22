eb1b935562dd496655ceeb4eefda396b50faac9f
Ho Thi Thu Tram <21087811.tram@student.iuh.edu.vn>
2024-08-22 08:00:47 +0700

    Answer c Exer1

--- exer1.js
+++ exer1.js
@@ -9,3 +9,6 @@ function calBMI(mass, height) {
 }
 console.log(`Mark's BMI ${calBMI(Mark_mass,Mark_height)}`)
 console.log(`John's BMI ${calBMI(John_mass,John_height)}`)
+
+markHigherBMI = (calBMI(Mark_mass, Mark_height) > calBMI(John_mass, John_height))
+console.log(markHigherBMI)
