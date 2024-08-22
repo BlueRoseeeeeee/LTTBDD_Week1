e9defa75e273c9185025e46e1652a0430119038b
Ho Thi Thu Tram <21087811.tram@student.iuh.edu.vn>
2024-08-22 07:50:43 +0700

    Week1_JS_Exer1_calcBMI

---
+++ exer1.js
@@ -0,0 +1,11 @@
+Mark_height = 1.69
+John_height = 1.88
+Mark_mass = 78
+John_mass = 95
+
+function calBMI(mass, height) {
+    BMI = mass / (height * height)
+    return BMI;
+}
+console.log(`Mark's BMI ${calBMI(Mark_mass,Mark_height)}`)
+console.log(`John's BMI ${calBMI(John_mass,John_height)}`)
